864e9d16018886b310d67f2a361c623fb0b11b7f
ZakYeo <zakyeomanson@gmail.com>
2023-10-09 21:07:53 +0100

    Added second option (.dat file content search)

--- file_search.py
+++ file_search.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import logging
+import xml.etree.ElementTree as ET
 from fuzzywuzzy import fuzz
 
 # Configure logging
@@ -30,12 +31,45 @@ def copy_file(source_path, destination_path):
         logging.error(f"Error copying file: {e}")
         return False
 
+def search_file_contents(directory, query, threshold=80):
+    """Search for .dat files based on a fuzzy match to their machine name contents."""
+    matches = []
+    for root, dirs, files in os.walk(directory):
+        for filename in files:
+            # Only consider .dat files
+            if not filename.lower().endswith(".dat"):
+                continue
+
+            filepath = os.path.join(root, filename)
+            try:
+                tree = ET.parse(filepath)
+                root_element = tree.getroot()
+
+                for machine in root_element.findall('machine'):
+                    machine_name = machine.get('name', '')
+                    ratio = fuzz.partial_ratio(query.lower(), machine_name.lower())
+                    if ratio > threshold:
+                        matches.append(filepath)
+                        break  # If one match is found in the file, no need to continue searching in it
+
+            except Exception as e:
+                logging.error(f"Error reading or parsing file {filepath}: {e}")
+    return matches
+
 def main():
+    search_type = input("Choose search type: \n1. By file name \n2. By .dat file contents\nEnter choice (1 or 2): ")
+    if search_type not in ['1', '2']:
+        logging.error("Invalid choice. Exiting.")
+        return
+
     query = input("Enter your search query: ")
     search_dir = input("Enter the directory to search in: ")
     copy_dir = input("Enter the directory to copy files to: ")
-    file_extension = input("Enter a file extension (e.g. '.txt') or leave blank for all extensions: ")
 
+    if search_type == '1':
+        file_extension = input("Enter a file extension (e.g. '.txt') or leave blank for all extensions: ")
+    else:  # If option 2, the file extension is implicitly .dat
+        file_extension = '.dat'
     # Allow user to specify the threshold
     try:
         threshold = int(input("Enter a threshold (0-100, default is 80, lower is more lenient): "))
@@ -45,13 +79,16 @@ def main():
     except ValueError:
         logging.warning("Invalid threshold input. Using the default of 80.")
         threshold = 80
+
+    if search_type == '1':
+        matched_files = search_files(search_dir, query, file_extension, threshold)
+    else:  # For .dat files and their XML content
+        matched_files = search_file_contents(search_dir, query, threshold)
     
     # Ensure the copy directory exists
     if not os.path.exists(copy_dir):
         os.makedirs(copy_dir)
 
-    # Search for files
-    matched_files = search_files(search_dir, query, file_extension, threshold)
     if not matched_files:
         logging.info("No files matched the search query.")
         return
